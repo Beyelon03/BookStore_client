1399ab6754b44d115474cdfb112b2de291f0b0f0
Beyelon <118230672+Beyelon03@users.noreply.github.com>
2024-08-04 22:45:08 +0300

    fix loading

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,18 +1,23 @@
 import { FC, useEffect } from 'react';
 import { Header } from '../Header/Header.tsx';
 import { Outlet } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from '../../store/store.ts';
-import { checkAuth } from '../../store/auth/auth.slice.ts';
+import { checkAuth, selectLoading } from '../../store/auth/auth.slice.ts';
 
 export const Layout: FC = () => {
   const dispatch: AppDispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
 
   useEffect(() => {
     if (localStorage.getItem('token')) {
       dispatch(checkAuth());
     }
   }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Загрузка страницы...</div>;
+  }
 
   return (
     <>
